171da902932fce724d02be3c6061b6f1808e5cf8
Cristina <cjimenez922@gmail.com>
2023-11-29 16:23:39 +0100

    creating Client class

---
+++ CJBank/src/com/cjbank/client/Client.java
@@ -0,0 +1,45 @@
+package com.cjbank.client;
+
+public class Client {
+	// 1.1.1 Creation of the client class
+	private String name;
+	private String firstname;
+	private int clientNumber;
+	private static int totalClients = 0;
+
+	public Client(String name, String firstname) {
+		this.name = name;
+		this.firstname = firstname;
+		this.clientNumber = totalClients++;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public int getClientNumber() {
+		return clientNumber;
+	}
+
+	public void setClientNumber(int clientNumber) {
+		this.clientNumber = clientNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [name=" + name + ", firstname=" + firstname + ", clientNumber=" + clientNumber + "]";
+	}
+
+}
