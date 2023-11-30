5f190aada1d6016916f1382abb512686a509b577
Cristina <cjimenez922@gmail.com>
2023-11-30 12:46:23 +0100

    create CurrentAccount and SavingsAccount

---
+++ CJBank/src/com/cjbank/client/account/CurrentAccount.java
@@ -0,0 +1,11 @@
+package com.cjbank.client.account;
+
+import com.cjbank.client.Client;
+
+public class CurrentAccount extends Account {
+	// 1.2.2 Creation of the CurrentAccount
+	public CurrentAccount(String label, Client client) {
+		super(label, client);
+	}
+
+}
---
+++ CJBank/src/com/cjbank/client/account/SavingsAccount.java
@@ -0,0 +1,11 @@
+package com.cjbank.client.account;
+
+import com.cjbank.client.Client;
+
+public class SavingsAccount extends Account {
+	// 1.2.2 Creation of the SavingsAccount
+	public SavingsAccount(String label, Client client) {
+		super(label, client);
+	}
+
+}
