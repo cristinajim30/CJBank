be7a8201ecb0b63ba709fb6caffc22450a159c37
Cristina <cjimenez922@gmail.com>
2023-11-30 12:36:03 +0100

    fix Collection, generateClient and loadClients for test class

--- CJBank/src/com/cjbank/main/Test.java
+++ CJBank/src/com/cjbank/main/Test.java
@@ -1,6 +1,8 @@
 package com.cjbank.main;
 
-import java.util.Arrays;
+import java.util.Collection;
+//import java.util.HashSet;
+import java.util.LinkedHashSet;
 
 import com.cjbank.client.Client;
 
@@ -8,27 +10,43 @@ public class Test {
 	// 1.1.2 Creation of main class for tests
 
 	public static void main(String[] args) {
-		Client[] clientsArray = new Client[10];
+		/*
+		 * use a LinkedHashSet to avoid repeated elements and to maintain the order in
+		 * which they are inserted
+		 */
+		Collection<Client> clientsSet = new LinkedHashSet();
 
-		loadClients(clientsArray);
-		displayClients(clientsArray);
+		loadClients(clientsSet);
+		displayClients(clientsSet);
 
 	}
 
-	private static void loadClients(Client[] clientsArray) {
-		for (int i = 0; i < clientsArray.length; i++) {
-			clientsArray[i] = generateClient(i + 1);
+	private static void loadClients(Collection<Client> clientsSet) {
+		// create a temporal Array to save newClients array.
+		Collection<Client> tempSet = new LinkedHashSet();
+
+		// create 10 clients
+		for (int i = 0; i < 10; i++) {
+			tempSet = generateClient(clientsSet.size() + 1);
+			for (Client client : tempSet) {
+				clientsSet.add(client);
+			}
 		}
+
 	}
 
-	private static Client generateClient(int clientNumber) {
+	private static Collection<Client> generateClient(int clientNumber) {
+		Collection<Client> newClient = new LinkedHashSet();
+
 		String name = "name" + clientNumber;
 		String firstName = "firstname" + clientNumber;
-		return new Client(name, firstName);
+
+		newClient.add(new Client(name, firstName));
+		return newClient;
 	}
 
-	private static void displayClients(Client[] clientsArray) {
-		Arrays.stream(clientsArray).forEach(System.out::println);
+	private static void displayClients(Collection<Client> clientsSet) {
+		clientsSet.stream().forEach(client -> System.out.println(client.toString()));
 
 	}
 
