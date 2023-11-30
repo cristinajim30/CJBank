9e6e9ce7e2e60bcc54103fa64aacd48c314b46ff
Cristina <cjimenez922@gmail.com>
2023-11-30 13:32:38 +0100

    create 1.2.3 execise

--- CJBank/src/com/cjbank/main/Test.java
+++ CJBank/src/com/cjbank/main/Test.java
@@ -1,23 +1,28 @@
 package com.cjbank.main;
 
 import java.util.Collection;
-//import java.util.HashSet;
 import java.util.LinkedHashSet;
 
 import com.cjbank.client.Client;
+import com.cjbank.client.account.Account;
+import com.cjbank.client.account.CurrentAccount;
+import com.cjbank.client.account.SavingsAccount;
 
 public class Test {
-	// 1.1.2 Creation of main class for tests
 
 	public static void main(String[] args) {
-		/*
-		 * use a LinkedHashSet to avoid repeated elements and to maintain the order in
-		 * which they are inserted
-		 */
+		// 1.1.2 Creation of main class for tests
+
+		// use a LinkedHashSet to avoid repeated elements and to maintain the order in
+		// which they are inserted
 		Collection<Client> clientsSet = new LinkedHashSet();
 
 		clientsSet = loadClients();
 		displayClients(clientsSet);
+
+		// 1.2.3 Creation of the tablea account
+		Collection<Account> accounts = loadAccounts(clientsSet);
+		displayAccounts(accounts);
 	}
 
 	private static Collection<Client> loadClients() {
@@ -42,6 +47,26 @@ public class Test {
 	private static void displayClients(Collection<Client> clientsSet) {
 		clientsSet.stream().forEach(client -> System.out.println(client.toString()));
 
+	}
+
+	private static Collection<Account> loadAccounts(Collection<Client> clientsSet) {
+		Collection<Account> accounts = new LinkedHashSet();
+
+		for (Client client : clientsSet) {
+			// Generate a saving account with zero balance
+			SavingsAccount savingsAccount = new SavingsAccount("Savings", client);
+			accounts.add(savingsAccount);
+
+			// Generate a current account with zero balance
+			CurrentAccount currentAccount = new CurrentAccount("Current", client);
+			accounts.add(currentAccount);
+		}
+
+		return accounts;
+	}
+
+	public static void displayAccounts(Collection<Account> accounts) {
+		accounts.stream().forEach(account -> System.out.println(account.toString()));
 	}
 
 }
